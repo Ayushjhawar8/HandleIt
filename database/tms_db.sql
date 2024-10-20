7a734268fe1e596715e1cd44414e8285ab01c592
Ayush Jhawar <111112495+Ayushjhawar8@users.noreply.github.com>
2024-10-20 12:54:52 +0000

    properly format all files

--- database/tms_db.sql
+++ database/tms_db.sql
@@ -3,8 +3,6 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2020 at 08:37 AM
--- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
